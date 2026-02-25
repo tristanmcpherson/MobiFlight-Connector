5d932e8f5995518d4c5745d3c514c42b4e01335a
tristanmcpherson <shredder8910@gmail.com>
2026-02-24 22:28:23 -0500

    Fix InputAction placeholder replacement for FSUIPC string offsets

--- MobiFlight/InputConfig/FsuipcOffsetInputAction.cs
+++ MobiFlight/InputConfig/FsuipcOffsetInputAction.cs
@@ -81,7 +81,8 @@ namespace MobiFlight.InputConfig
 
             if (value.Contains("@"))
             {
-                Tuple<string, string> replacement = new Tuple<string, string>("@", args.Value.ToString());
+                var inputValue = !string.IsNullOrEmpty(args.StrValue) ? args.StrValue : args.Value.ToString();
+                Tuple<string, string> replacement = new Tuple<string, string>("@", inputValue);
                 replacements.Add(replacement);
             }
 
@@ -179,7 +180,7 @@ namespace MobiFlight.InputConfig
             }
             else if (FSUIPC.Size == 255)
             {
-                cache.setOffset(FSUIPC.Offset, Value);
+                cache.setOffset(FSUIPC.Offset, value);
             }
 
             cache.Write();
